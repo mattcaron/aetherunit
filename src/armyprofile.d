665713e9510f273134d4cb0ceb36fe57366bd96a
Matthew Caron <matt@mattcaron.net>
2013-09-10 13:15:39 -0400

    Add base army profile class.

---
+++ src/armyprofile.d
@@ -0,0 +1,13 @@
+import utility.accessor_template;
+
+class armyProfile {
+
+
+    mixin declarationAndProperties!("int", "DEX");
+    mixin declarationAndProperties!("int", "STR");
+    mixin declarationAndProperties!("int", "CON");
+    mixin declarationAndProperties!("int", "TEK");
+    mixin declarationAndProperties!("int", "MOR");
+    mixin declarationAndProperties!("int", "PRE");
+
+}
