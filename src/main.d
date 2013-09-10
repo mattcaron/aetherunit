314066871b011a3abbdadd1b068d6b7b0a49a762
Matthew Caron <matt@mattcaron.net>
2013-09-10 13:15:14 -0400

    Rework main to be more palatable. Add FIXME.

--- src/main.d
+++ src/main.d
@@ -11,6 +11,7 @@ import gobject.Type;
 
 int main(string[] args) {
     string gladestring = import("main.glade");
+    int retVal = 1;
 
     Main.init(args);
 
@@ -19,30 +20,24 @@ int main(string[] args) {
     if(!g.addFromString(gladestring, gladestring.length)) {
         writefln("Oops, could not create Glade object, "
                  "check your glade file ;)");
-        exit(1);
-    }
-
-    Window w = cast(Window)g.getObject("window1");
-
-    if (w !is null) {
-        w.setTitle("This is a glade window");
-        w.addOnHide( delegate void(Widget aux){ exit(0); } );
-
-        Button b = cast(Button)g.getObject("button1");
-        if(b !is null) {
-            b.addOnClicked(
-                delegate void(Button aux){ 
-                    exit(0);
-                } 
-            );
-        }
     } else {
-        writefln("No window?");
-        exit(1);
+        Window w = cast(Window)g.getObject("window1");
+
+        if (w !is null) {
+            w.addOnHide(
+                delegate void(Widget aux) { 
+                    retVal = 0;
+                    // FIXME - only main should exit
+                    exit(retVal);
+                }
+            );
+        } else {
+            writefln("No window?");
+        }
+
+        w.showAll();
+        Main.run();
     }
 
-    w.showAll();
-    Main.run();
-
-    return 0;
+    return retVal;
 }
