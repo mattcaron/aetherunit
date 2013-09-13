e96e6c822e348aa9c0a037660f83c268c3ba85d2
Matthew Caron <matt@mattcaron.net>
2013-09-12 22:03:15 -0400

    Add army cost calculation function.

--- src/armyProfile.d
+++ src/armyProfile.d
@@ -1,6 +1,15 @@
 import utility.accessorTemplate;
+import utility.debugPrint;
 
 class armyProfile {
+
+    private int summation(int x) {
+        int temp = 0;
+        foreach (int i; 0..x+1) {
+            temp += i;
+        }
+        return temp;
+    }
 
     mixin declarationAndProperties!("int", "DEX");
     mixin declarationAndProperties!("int", "STR");
@@ -8,4 +17,12 @@ class armyProfile {
     mixin declarationAndProperties!("int", "TEK");
     mixin declarationAndProperties!("int", "MOR");
     mixin declarationAndProperties!("int", "PRE");
+    mixin declarationAndProperties!("int", "armyBaseCost");
+
+    int recalculate() {
+        armyBaseCost = summation(DEX) + summation(STR) + summation(CON) + 
+                       summation(TEK) + summation(MOR) + summation(PRE);
+        return armyBaseCost;
+    }
+
 }
