de6f43ac8979a792233cacc302fe67aa17afc373
Matthew Caron <matt@mattcaron.net>
2013-09-27 12:52:03 -0400

    Added base vehicle class.

---
+++ src/models/vehicle.d
@@ -0,0 +1,37 @@
+module models.vehicle;
+
+import models.armyProfile;
+import models.supportUnit;
+
+/**
+ *  An abstract class representing a vehicle, which is always a support unit.
+ */
+abstract class vehicle : supportUnit {
+
+    /**
+     * Vehicles are single model support units
+     *
+     * TEK is the index in to the array.
+     */
+    immutable int[] baseModelArray = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+    /**
+     * Number of stat points units get to spend to increase stats
+     */
+    immutable int extraStatPoints = 0;
+
+    /**
+     * Initializing constructor
+     *
+     * @FIXME - make readonly
+     *
+     * @param profile reference to profile on which this unit is
+     *                based.
+     */
+    this(armyProfile profile) {
+        super(profile);
+        type = unitType.Support;
+    }
+
+
+}
