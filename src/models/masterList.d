69ad0f369b939620841a34f748900090f67f59f8
Matthew Caron <matt@mattcaron.net>
2013-10-06 19:17:04 -0400

    Add generic path decoding function.

--- src/models/masterList.d
+++ src/models/masterList.d
@@ -1,7 +1,11 @@
 module models.masterList;
 
+import std.conv;
+import std.regex;
+
 import utility.accessorTemplate;
 
+import models.aetherVerseObject;
 import models.basicUnit;
 import models.supportUnit;
 import models.eliteUnit;
@@ -11,7 +15,7 @@ import models.meleeWeapon;
 
 /**
  * A "master" list class consisting of a bunch of arrays of all the
- * other possible aetherverseObject subtyptes
+ * other possible aetherVerseObject subtyptes
  */
 class masterList {
 
@@ -49,6 +53,66 @@ class masterList {
      * Default constructor
      */
     this() {
+    }
+
+    /**
+     * Get the object to which a TreeView coordinates "path" points
+     *
+     * A TreeView path consists of a series of integer indices (0
+     * based) separated by commas. So, 2,4 is the fifth child of the
+     * third node. We populate the menu as follows:
+     * 
+     * - Basic Units
+     * - Elite Units
+     * - Support Units
+     * - Armors
+     * - Ranged Weapons
+     * - Melee Weapons
+     *
+     * @param path the path to be decoded
+     *
+     * @return the aetherVerseObject pointed to by the path, or null
+     *         if it's none of those
+     */
+    aetherVerseObject getObjectFromPath(string path) {
+        aetherVerseObject retVal = null;
+        int[] indices;
+        string[] splitStrings = split(path, regex(r",\D*"));
+        foreach (string splitString; splitStrings) {
+            indices ~= to!int(splitString);
+        }
+
+        if (indices.length > 1) {
+            // The top level is the heading, the next level is the
+            // index in to it
+            switch (indices[0]) {
+            case 0:
+                retVal = basicUnits[indices[1]];
+                break;
+            case 1:
+                retVal = eliteUnits[indices[1]];
+                break;
+            case 2:
+                retVal = supportUnits[indices[1]];
+                break;
+            case 3:
+                retVal = armors[indices[1]];
+                break;
+            case 4:
+                retVal = rangedWeapons[indices[1]];
+                break;
+            case 5:
+                retVal = meleeWeapons[indices[1]];
+                break;
+            default:
+                // retVal is already null
+                break;
+            }
+        }
+        // else, it's == 1 so it's a heading or nothing is selected,
+        // just set the add button
+
+        return retVal;
     }
 
 }
